63bc1be9dc3151ec602333434f69cf6640bd6fd4
Bram <bram@bram.codes>
2018-05-16 16:51:10 +0200

    Just a working prototype
    * Fix tests
    * Log
    * Test log
    * Travis
    * Codecov

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const winston = require('winston');
 const util = require('util');
+const https = require('https');
 
-const endpoint = 'https://discordapp.com/api/v6/webhooks/';
-const webhookURLRegex = /(https?):\/\/([(a-z0-9).]+)\/api(?:\/v([0-9]+))*\/webhooks\/([0-9]{1,20})\/([a-z0-9_-]+)/gi;
-
-exports = module.exports = DiscordLogger = function(options) {
+module.exports = DiscordLogger = winston.transports.DiscordLogger = function(options) {
 	this.name = options.name || 'DiscordLogger';
 	this.level = options.level || 'info';
 
@@ -13,13 +11,12 @@ exports = module.exports = DiscordLogger = function(options) {
 		webhooks.push(options.webhooks)
 	} else if (Array.isArray(options.webhooks)) {
 		webhooks = options.webhooks;
-	} else {
-		return new Error('Invalid webhooks format');
 	}
 
 	webhooks = webhooks.map(webhook => {
 		if (typeof webhook === 'string') {
-			const match = options.webhooks.match(webhookURLRegex);
+			const webhookURLRegex = /(https?):\/\/([(a-z0-9).]+)\/api(?:\/v([0-9]+))*\/webhooks\/([0-9]{1,20})\/([a-z0-9_-]+)/gi;
+			const match = webhookURLRegex.exec(webhook);
 			if (match) {
 				return { 'id': match[4], 'token': match[5] };
 			} else {
@@ -32,14 +29,49 @@ exports = module.exports = DiscordLogger = function(options) {
 		return undefined;
 	});
 
-	console.log(webhooks);
-
 	this.webhooks = webhooks;
-	return this.webhooks;
 };
 
 util.inherits(DiscordLogger, winston.Transport);
 
 DiscordLogger.prototype.log = function (level, msg, meta, callback) {
+	// Make message
 
+	const promises = this.webhooks.map(webhook => {
+		return new Promise((resolve, reject) => {
+			const body = JSON.stringify({
+				content: msg
+			});
+
+			const post = {
+				hostname: 'discordapp.com',
+				path: `/api/v6/webhooks/${webhook.id}/${webhook.token}`,
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Content-Length': Buffer.byteLength(body)
+				}
+			};
+
+			const request = https.request(post, result => {
+				result.setEncoding('utf8');
+				result.on('data', chunk => {
+					console.log('Response: ' + chunk);
+				});
+			});
+
+			request.write(body, 'utf8', result => {
+				console.log(result);
+				return resolve(result);
+			});
+		});
+	});
+
+	Promise.all(promises).then(results => {
+		console.log(results);
+
+		callback(null, level, msg, meta);
+	}).catch(error => {
+		callback(error);
+	});
 };
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+const process = require('process');
 const test = require('ava');
 const winston = require('winston');
 const DiscordLogger = require('./index');
@@ -5,37 +6,71 @@ const DiscordLogger = require('./index');
 const webhooksByUrl = require('./webhooks-by-url');
 const webhooksByIdAndToken = require('./webhooks-by-id-and-token');
 
-winston.remove(winston.transports.Console);
-
 test('accept webhook url', t => {
-	const result = winston.add(DiscordLogger, { webhooks: webhooksByUrl[0] });
+	const logger = new (winston.Logger)({
+		transports: [
+			new (DiscordLogger)({ webhooks: webhooksByUrl[0] })
+		]
+	});
 
-	t.is(result, { 'id': '446240708299587584', 'token': 'W_y7cmhCv2KY-oKopLHFTTw08L0apQVS0bkrUDHV44Es4Vb-p3Z4uEEHEwpVkHVjpaqg' });
+	const webhooks = logger.transports.DiscordLogger.webhooks;
+
+	t.is(webhooks[0].id, '446240708299587584');
+	t.is(webhooks[0].token, 'W_y7cmhCv2KY-oKopLHFTTw08L0apQVS0bkrUDHV44Es4Vb-p3Z4uEEHEwpVkHVjpaqg');
 });
 
 test('accept array of webhook urls', t => {
-	const result = winston.add(DiscordLogger, { webhooks: webhooksByUrl });
+	const logger = new (winston.Logger)({
+		transports: [
+			new (DiscordLogger)({ webhooks: webhooksByUrl })
+		]
+	});
 
-	t.is(result, [
-		{ 'id': '446240708299587584', 'token': 'W_y7cmhCv2KY-oKopLHFTTw08L0apQVS0bkrUDHV44Es4Vb-p3Z4uEEHEwpVkHVjpaqg' },
-		{ 'id': '446241166493483008', 'token': 'JeOKubEIRAcalZISmjGrG6DuzHX_KdE5Uzq_r0mK0318voc_7TRjC6NA_oe-fYX_eAhn' }
-	]);
+	const webhooks = logger.transports.DiscordLogger.webhooks;
+
+	t.is(webhooks[0].id, '446240708299587584');
+	t.is(webhooks[0].token, 'W_y7cmhCv2KY-oKopLHFTTw08L0apQVS0bkrUDHV44Es4Vb-p3Z4uEEHEwpVkHVjpaqg');
+	t.is(webhooks[1].id, '446241166493483008');
+	t.is(webhooks[1].token, 'JeOKubEIRAcalZISmjGrG6DuzHX_KdE5Uzq_r0mK0318voc_7TRjC6NA_oe-fYX_eAhn');
 });
 
 test('accept webhook id and token object', t => {
-	const result = winston.add(DiscordLogger, { webhooks: webhooksByIdAndToken[0] });
-	t.is(result, { 'id': '446240708299587584', 'token': 'W_y7cmhCv2KY-oKopLHFTTw08L0apQVS0bkrUDHV44Es4Vb-p3Z4uEEHEwpVkHVjpaqg' });
+	const logger = new (winston.Logger)({
+		transports: [
+			new (DiscordLogger)({ webhooks: webhooksByIdAndToken[0] })
+		]
+	});
+
+	const webhooks = logger.transports.DiscordLogger.webhooks;
+
+	t.is(webhooks[0].id, '446240708299587584');
+	t.is(webhooks[0].token, 'W_y7cmhCv2KY-oKopLHFTTw08L0apQVS0bkrUDHV44Es4Vb-p3Z4uEEHEwpVkHVjpaqg');
 });
 
 test('accept array of webhook id and token objects', t => {
-	const result = winston.add(DiscordLogger, { webhooks: webhooksByIdAndToken });
-	t.is(result, [
-		{ 'id': '446240708299587584', 'token': 'W_y7cmhCv2KY-oKopLHFTTw08L0apQVS0bkrUDHV44Es4Vb-p3Z4uEEHEwpVkHVjpaqg' },
-		{ 'id': '446241166493483008', 'token': 'JeOKubEIRAcalZISmjGrG6DuzHX_KdE5Uzq_r0mK0318voc_7TRjC6NA_oe-fYX_eAhn' }
-	]);
+	const logger = new (winston.Logger)({
+		transports: [
+			new (DiscordLogger)({ webhooks: webhooksByIdAndToken })
+		]
+	});
+
+	const webhooks = logger.transports.DiscordLogger.webhooks;
+
+	t.is(webhooks[0].id, '446240708299587584');
+	t.is(webhooks[0].token, 'W_y7cmhCv2KY-oKopLHFTTw08L0apQVS0bkrUDHV44Es4Vb-p3Z4uEEHEwpVkHVjpaqg');
+	t.is(webhooks[1].id, '446241166493483008');
+	t.is(webhooks[1].token, 'JeOKubEIRAcalZISmjGrG6DuzHX_KdE5Uzq_r0mK0318voc_7TRjC6NA_oe-fYX_eAhn');
 });
 
-// test('don\'t accept urls that are not from discord', t => {
-//
-// });
+test.cb('send simple log message', t => {
+	const logger = new (winston.Logger)({
+		transports: [
+			new (DiscordLogger)({ webhooks: { id: process.env.WEBHOOK_ID, token: process.env.WEBHOOK_TOKEN } })
+		]
+	});
 
+	logger.log('error', 'This is a simple log message', (error, level, message, meta) => {
+		t.is(message, 'This is a simple log message');
+		t.end();
+	});
+});
